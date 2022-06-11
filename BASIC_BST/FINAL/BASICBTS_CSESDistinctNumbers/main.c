55fa485c7db64dbf6476c680d45f27a68e015aa3
nvkuy <nguyenvukhanhuy@gmail.com>
2022-06-11 22:31:07 +0700

    Finished basic BST, making RBT

--- BASIC_BST/FINAL/BASICBTS_CSESDistinctNumbers/main.c
+++ BASIC_BST/FINAL/BASICBTS_CSESDistinctNumbers/main.c
@@ -176,17 +176,36 @@ int getSize() {
     return treeSize;
 }
 
+void shuffle(int *array, size_t n)
+{
+    if (n > 1)
+    {
+        size_t i;
+        for (i = 0; i < n - 1; i++)
+        {
+          size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
+          int t = array[j];
+          array[j] = array[i];
+          array[i] = t;
+        }
+    }
+}
+
+int a[200002];
+
 int main()
 {
 
     newTree();
-    int n, ai;
+    int n;
 
     scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &ai);
-        addNode(ai, 0);
-    }
+    for (int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    shuffle(a, n);
+    for (int i = 0; i < n; i++)
+        addNode(a[i], 0);
+
     printf("%d", getSize());
 
 }
